b3d55ff7f4d52c95a5872244146d38071fa61754
sergivirgili <sergi.virgili.alonso@gmail.com>
2025-04-26 12:27:12 -0600

    created daily.py

--- backend/app/daily.py
+++ backend/app/daily.py
@@ -10,3 +10,16 @@ questions ={}
 temp_streak = {}
 
 @router.get("/daily-question")
+def get_daily_question():
+    today = str(date.today())
+    question_data = questions.get(today)
+    if question_data:
+        return {"question": question_data["questions"], 
+                "options" : question_data["options"]}
+    else:
+        return {"message": "No question available for today."}
+    
+@router.post("/submit-daily-answer")
+def submit_daily_answer(username:str, answer:str):
+    today = str(date.tpday())
+    correct_answer = questions.get(today, {}).get("answer")
