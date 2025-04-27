6726e0b112cd85ab378b18425d899de3debd83cb
Noel Lozano <noelmlozano6@gmail.com>
2025-04-27 04:48:45 -0600

    changes

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -29,7 +29,7 @@ class LoginRequest(BaseModel):
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
